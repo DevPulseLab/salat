83be64006f2b54b67afa7184d37186cc5915d546
Georg Spack <georgynet@gmail.com>
2024-09-21 19:14:12 +0200

    fix annotations

--- internal/dto/claims.go
+++ internal/dto/claims.go
@@ -3,6 +3,6 @@ package dto
 import "github.com/golang-jwt/jwt/v5"
 
 type Claims struct {
-	Username string `json:"username`
+	Username string `json:"username"`
 	jwt.RegisteredClaims
 }
--- internal/forms/login_form.go
+++ internal/forms/login_form.go
@@ -1,6 +1,6 @@
 package forms
 
 type LoginForm struct {
-	Username string `form:"username binding: "required" json:"username"`
-	Password string `form:"password binding: "required" json:"password"`
+	Username string `form:"username" binding: "required" json:"username"`
+	Password string `form:"password" binding: "required" json:"password"`
 }
