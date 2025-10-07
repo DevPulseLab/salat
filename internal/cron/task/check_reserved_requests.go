859c25cfd4c3d0594c330638e98f72e61ca82c4f
Georg Spack <georgynet@gmail.com>
2025-10-07 17:40:26 +0200

    split calendar repo into repo and service

--- internal/cron/task/check_reserved_requests.go
+++ internal/cron/task/check_reserved_requests.go
@@ -3,7 +3,6 @@ package task
 import (
 	"github.com/DevPulseLab/salat/internal/config"
 	"github.com/DevPulseLab/salat/internal/db/repositories"
-	"github.com/DevPulseLab/salat/internal/helper"
 	"github.com/DevPulseLab/salat/internal/services"
 	"github.com/sirupsen/logrus"
 	"github.com/uniplaces/carbon"
@@ -19,13 +18,18 @@ type CheckReservedRequests struct {
 
 func NewCheckReservedRequests(config *config.Config, db *gorm.DB, logger *logrus.Logger) *CheckReservedRequests {
 	ms := services.NewMessagingService(config.Slack.Token, db)
-	calendarRepo := repositories.NewCalendarRepository(db, helper.NewDateHelper())
+	calendarRepo := repositories.NewCalendarRepository(db)
 	return &CheckReservedRequests{config, ms, calendarRepo, logger}
 }
 
 func (task *CheckReservedRequests) Execute() {
 	now := carbon.Now()
-	countReservedRequests := task.CalendarRepo.CountReservedForDate(now)
+	countReservedRequests, err := task.CalendarRepo.CountReservedByDate(now)
+	if err != nil {
+		countReservedRequests = 0
+		task.Logger.Errorf("Error while count reserved by date: %s", err.Error())
+	}
+
 	task.Logger.Debugf("Count reserved requests: %d", countReservedRequests)
 
 	if countReservedRequests > 0 {
--- internal/handlers/admin_calendar_handler.go
+++ internal/handlers/admin_calendar_handler.go
@@ -28,9 +28,8 @@ type AdminCalendarHandler struct {
 }
 
 func NewAdminCalendarHandler(db *gorm.DB, config *config.Config, log *logrus.Logger) *AdminCalendarHandler {
-	dateHelper := helper.NewDateHelper()
 	closeIntervalRepo := repositories.NewCloseIntervalsRepository(db)
-	calendarRepo := repositories.NewCalendarRepository(db, dateHelper)
+	calendarRepo := repositories.NewCalendarRepository(db)
 	visitStatsRepo := repositories.NewVisitStatsRepository(db)
 	requestHelper := helper.NewRequestHelper()
 	calendarDtoBuilder := builder.NewCalendarDtoBuilder()
@@ -60,7 +59,12 @@ func (handler *AdminCalendarHandler) AllUserList(ctx *gin.Context) {
 		return
 	}
 
-	calendars := handler.CalendarRepo.GetCalendarEntriesForAllUsers(startDate, endDate)
+	calendars, err := handler.CalendarRepo.FindByDateRange(startDate, endDate)
+	if err != nil {
+		handler.Logger.Errorf("Errror while find data by date range: %s", err.Error())
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "An error occurred data by range"})
+		return
+	}
 
 	calendarDtos := handler.CalendarDtoBuilder.BuildFromCalendarModels(calendars)
 
@@ -74,7 +78,7 @@ func (handler *AdminCalendarHandler) ChangeEntryStatus(ctx *gin.Context) {
 		return
 	}
 
-	calendarModel, err := handler.CalendarRepo.ChangeEntryStatus(form.CalendarEntryId, form.NewStatus)
+	calendarModel, err := handler.CalendarRepo.UpdateStatus(form.CalendarEntryId, form.NewStatus)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Calendar entry not changed"})
 		return
